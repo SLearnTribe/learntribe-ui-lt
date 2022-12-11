32b16e05ce7185d3598c50dcb0f20a8812231f83
Adil <adil.c@imperva.com>
2022-12-11 11:20:38 +0530

    Removed unmount logic for profile

--- src/Ts/Components/Pages/Profiles/CandidateProfile/Profile.jsx
+++ src/Ts/Components/Pages/Profiles/CandidateProfile/Profile.jsx
@@ -1,21 +1,9 @@
 import { Grid } from "@mui/material";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { updateUserProfile } from "../../../../Redux/Ducks/Profile/ProfileSlice";
-import { getUserProfileInfo } from "../../../../Redux/Selectors/ProfileSelectors/ProfileSelectors";
+import React from "react";
 import { ProfileAvatarSection } from "../../../Candidate/Profile/ProfileAvtarSection";
 import { ProfileContentSection } from "../../../Candidate/Profile/ProfileContentSection";
 
 export const CandidateProfile = () => {
-  const dispatch = useDispatch();
-
-  const userInfo = useSelector(getUserProfileInfo);
-
-  useEffect(() => {
-    return () => {
-      dispatch(updateUserProfile(userInfo));
-    };
-  }, [dispatch, userInfo]);
   return (
     <Grid container spacing={3}>
       <ProfileAvatarSection />
