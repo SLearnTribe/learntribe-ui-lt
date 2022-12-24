2652bae8bd093b32eab1348636ab3ef64005923f
Adil <adil.c@imperva.com>
2022-12-24 14:56:54 +0530

    skeleton test

--- src/Ts/Components/Pages/Dashboards/Candidate/StatCards.jsx
+++ src/Ts/Components/Pages/Dashboards/Candidate/StatCards.jsx
@@ -4,23 +4,33 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { CandidateActivitiesIconMap } from "../../../../Configs/Dashboards/DashboardsConfig";
 import { getCandidateActivitiesData } from "../../../../Redux/Selectors/Dashboard/CandidateDashboardSelectors";
+import { getIsUserDataLoading } from "../../../../Redux/Selectors/UserSelectors/UserSelectors";
+import { CandidateActivitiesSkeleton } from "../../../../Skeletons/CardSkeletons";
 import DashboardStatsCard from "../../../CommonComponents/StatsCard";
 
 export const StatCards = () => {
   const activities = useSelector(getCandidateActivitiesData);
 
+  const isLoading = useSelector(getIsUserDataLoading);
+
   return (
     <>
-      {activities.map(({ title, total, color }) => (
-        <Grid key={uniqueId()} item xs={12} sm={6} md={4}>
-          <DashboardStatsCard
-            title={title}
-            total={total}
-            color={color}
-            Icon={CandidateActivitiesIconMap[title]}
-          />
-        </Grid>
-      ))}
+      {isLoading && 0 ? (
+        <CandidateActivitiesSkeleton />
+      ) : (
+        <>
+          {activities.map(({ title, total, color }) => (
+            <Grid key={uniqueId()} item xs={12} sm={6} md={4}>
+              <DashboardStatsCard
+                title={title}
+                total={total}
+                color={color}
+                Icon={CandidateActivitiesIconMap[title]}
+              />
+            </Grid>
+          ))}
+        </>
+      )}
     </>
   );
 };
